f542d1c54df0d7c027a120cd49a32b553062442e
IHIutch <jbhutch01@gmail.com>
2020-03-17 21:11:08 -0400

    Show preview card

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -9,109 +9,156 @@
 
     <b-container>
       <b-row>
-        <b-col class="col-md-6 offset-md-3 mt-5 pt-5 pb-4">
-          <h1 class="mt-5">Add a local business</h1>
-          <p>
-            Enter the local business to add them on the list!
-          </p>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col class="col-md-6 offset-md-3">
-          <b-form @submit.stop.prevent="sendFormData" autocomplete="off">
-            <b-form-group label="Company Name" label-for="company">
-              <b-form-input
-                id="company"
-                v-model="form.company"
-                required
-                placeholder="Local business name... "
-                autofocus
-              ></b-form-input>
-            </b-form-group>
-            <div class="row">
-              <div class="col-6">
-                <b-form-group label="Stage" label-for="stage">
-                  <b-form-select
-                    id="stage"
-                    v-model="form.stage"
-                    :options="stages"
-                    required
-                  >
-                    <template v-slot:first>
-                      <b-select-option :value="null" disabled=""
-                        >-- Select a Stage --</b-select-option
-                      >
-                    </template>
-                  </b-form-select>
-                </b-form-group>
-              </div>
-            </div>
-            <b-form-group label="Gift Cards" label-for="website">
-              <b-form-input
-                id="website"
-                v-model="form.website"
-                required
-                placeholder="Gift Card Link... "
-              ></b-form-input>
-            </b-form-group>
-            <b-form-group label="Description" label-for="description">
-              <b-form-textarea
-                id="description"
-                v-model="form.description"
-                required
-                placeholder="Briefly describe the startup..."
-                rows="3"
-                no-resize
-              ></b-form-textarea>
-            </b-form-group>
-
-            <b-form-group label="Your Email" label-for="email">
-              <b-form-input
-                id="email"
-                v-model="form.email"
-                type="email"
-                required
-                placeholder="Best email for verification..."
-              ></b-form-input>
-            </b-form-group>
-            <div class="text-center mb-5">
-              <b-button
-                type="submit"
-                variant="primary"
-                class="mt-3 mb-5"
-                :disabled="saving"
-              >
-                <span v-if="saving">
-                  <b-spinner small></b-spinner>
-                  Saving...
-                </span>
-                <span v-else>Submit</span>
-              </b-button>
-              <p class="lightgrey">
-                By clicking Submit you agree that the information listed above
-                is true to the best of your knowledge. You're also saying it's
-                cool if we email you. We won't spam you so don't worry, be
-                happy. Now, submit your Startup already.
+        <b-col class="col-md-10 offset-md-1">
+          <b-row class="mt-5">
+            <b-col class="pb-4">
+              <h1>Add a local business</h1>
+              <p>
+                Enter the local business to add them on the list!
               </p>
-              <br />
-              <a
-                class="lightgrey"
-                href="https://www.helmux.com/work"
-                target="_blank"
-              >
-                Built with &#x2665;
-              </a>
-              <br />
-              <br />
-              <br />
-              <a
-                href="https://docs.google.com/forms/d/e/1FAIpQLScA9hirfSieN7Wii81gPZGP3_zvJAzg9K4Ye51MTT9M8-Ybmg/viewform?usp=sf_link"
-                target="_blank"
-              >
-                Feedback?
-              </a>
-            </div>
-          </b-form>
+            </b-col>
+          </b-row>
+
+          <b-row>
+            <b-col class="col-md-4 position-relative">
+              <div class="sticky-top pt-5 mt-n5">
+                <b-card no-body class="mb-2">
+                  <b-card-header
+                    :style="{
+                      backgroundColor: form.color ? form.color : '#222222'
+                    }"
+                    header-tag="header"
+                  >
+                    <div class="embed-responsive embed-responsive-21by9">
+                      <div
+                        class="embed-responsive-item d-flex align-items-center"
+                      >
+                        <b-img-lazy
+                          class="w-100 h-auto"
+                          src="https://placekitten.com/300/100"
+                          :alt="form.name ? form.name : 'Add a business name'"
+                        />
+                      </div>
+                    </div>
+                  </b-card-header>
+                  <b-card-body>
+                    <b-card-title>
+                      {{ form.name ? form.name : "Add a business name" }}
+                    </b-card-title>
+                    <b-card-text>
+                      {{
+                        form.description
+                          ? form.description
+                          : "Write a description"
+                      }}
+                    </b-card-text>
+                    <b-button
+                      block
+                      :href="form.pageUrl ? form.pageUrl : '#'"
+                      variant="primary"
+                      >Purchase Giftcard</b-button
+                    >
+                  </b-card-body>
+                </b-card>
+              </div>
+            </b-col>
+
+            <b-col class="col-md-7 offset-md-1">
+              <b-form @submit.stop.prevent="sendFormData" autocomplete="off">
+                <b-form-group label="Company Name" label-for="company">
+                  <b-form-input
+                    id="company"
+                    v-model="form.name"
+                    required
+                    placeholder="Local business name... "
+                    autofocus
+                  ></b-form-input>
+                </b-form-group>
+
+                <b-form-group label="Upload a logo" label-for="file-default">
+                  <b-form-file id="file-default"></b-form-file>
+                </b-form-group>
+
+                <b-form-group label="Select a color" label-for="color">
+                  <b-row>
+                    <b-col cols="auto">
+                      <input type="color" id="color" v-model="form.color" />
+                    </b-col>
+                    <b-col>
+                      <input
+                        class="form-control"
+                        type="text"
+                        :placeholder="form.color"
+                        readonly
+                      />
+                    </b-col>
+                  </b-row>
+                </b-form-group>
+
+                <b-form-group label="Gift Card Amounts" label-for="amount">
+                  <small class="form-text text-muted"
+                    >Or simply write "Any amount".</small
+                  >
+                  <b-input-group
+                    class="mb-2"
+                    v-for="(amount, idx) in form.amounts.length"
+                    :key="idx"
+                  >
+                    <b-form-input
+                      id="amount"
+                      v-model="form.amounts[idx]"
+                      required
+                      placeholder="$5.00"
+                    ></b-form-input>
+                    <b-input-group-append>
+                      <b-button
+                        v-if="form.amounts.length > 1"
+                        variant="info"
+                        @click="removeAmount(idx)"
+                        >Remove</b-button
+                      >
+                    </b-input-group-append>
+                  </b-input-group>
+                </b-form-group>
+                <b-button variant="primary" @click="addAmount()">
+                  Add Amount
+                </b-button>
+                <b-form-group
+                  label="Add a link to the gift card page"
+                  label-for="pageUrl"
+                >
+                  <b-form-input
+                    id="pageUrl"
+                    v-model="form.pageUrl"
+                    required
+                    placeholder="https://www.giftcard.com/"
+                  ></b-form-input>
+                </b-form-group>
+                <b-form-group label="Description" label-for="description">
+                  <b-form-textarea
+                    id="description"
+                    v-model="form.description"
+                    required
+                    placeholder="Briefly describe the form..."
+                    rows="3"
+                    no-resize
+                  ></b-form-textarea>
+                </b-form-group>
+                <b-button
+                  type="submit"
+                  variant="primary"
+                  class="mt-3 mb-5"
+                  :disabled="saving"
+                >
+                  <span v-if="saving">
+                    <b-spinner small></b-spinner>
+                    Saving...
+                  </span>
+                  <span v-else>Submit</span>
+                </b-button>
+              </b-form>
+            </b-col>
+          </b-row>
         </b-col>
       </b-row>
     </b-container>
@@ -130,20 +177,25 @@ export default {
         "https://script.google.com/macros/s/AKfycbwlb0OuUE_0_1xUtIAsUgJk5gKdHCbvQRMVcx10W6BhroY8i1lP/exec",
       form: {
         email: "",
-        business_name: "",
+        name: "",
         description: "",
         address: "",
-        amounts: "",
-        stage: null,
+        amounts: [""],
         type: "",
-        page_url: "",
+        pageUrl: "",
         logo: "",
-        color: ""
+        color: "#222222"
       },
       saving: false
     };
   },
   methods: {
+    addAmount() {
+      this.form.amounts.push("");
+    },
+    removeAmount(idx) {
+      this.form.amounts.splice(idx, 1);
+    },
     gtmTrackFormSubmit() {
       this.$gtm.trackEvent({
         event: "formSubmit",
@@ -171,47 +223,6 @@ export default {
           path: "/thanks"
         });
       });
-    },
-    getAddressValues(autocomplete) {
-      var place = autocomplete.getPlace();
-      for (var i = 0; i < place.address_components.length; i++) {
-        var key = place.address_components[i].types[0];
-        this.form.address[key] = place.address_components[i].long_name;
-        this.form.lat = place.geometry.location.lat();
-        this.form.lng = place.geometry.location.lng();
-      }
-    },
-    initAutocomplete() {
-      var self = this;
-      VueScript2.load(
-        `https://maps.googleapis.com/maps/api/js?key=${"AIzaSyC2tSkbaL8SIQsSQGlIsQZXn-wHBD3z-Rs"}&libraries=places`
-      ).then(function() {
-        var autocomplete = new google.maps.places.Autocomplete(
-          document.getElementById("autocomplete"),
-          { types: ["geocode"] }
-        );
-        autocomplete.setFields(["address_component", "geometry"]);
-        autocomplete.addListener("place_changed", function() {
-          self.getAddressValues(autocomplete);
-          self.validation = null;
-        });
-      });
-    }
-  },
-  mounted() {
-    let self = this;
-    this.initAutocomplete();
-    document
-      .getElementById("autocomplete")
-      .addEventListener("input", function() {
-        self.form.address = {};
-      });
-  },
-  computed: {
-    dataToArray() {
-      return Object.keys(this.form).map(key =>
-        key == "address" ? JSON.stringify(this.form[key]) : this.form[key]
-      );
     }
   }
 };
