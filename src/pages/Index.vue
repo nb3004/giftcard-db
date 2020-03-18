94b59eebd4da2d0331b6e0d1e680cb99a03d4aa2
IHIutch <jbhutch01@gmail.com>
2020-03-17 23:47:39 -0400

    Add searching and filtering

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,63 +7,59 @@
         >
       </b-container>
     </b-navbar>
-      
+
     <b-container>
-      <b-row>
-        <b-col cols="12" class="text-center hero">
-          
-          <g-link class="navbar-brand" to="/">Buffalo Gift Card Database</g-link>
+      <b-row class="my-5 py-5">
+        <b-col cols="12" class="text-center my-5">
+          <h1 class="text-green">Buffalo Gift Card Database</h1>
           <p>Lorem Ipsum</p>
-            
-          </b-col>
-        </b-row>
-      </b-container>
-
-      
-    <b-container class="padding-50">
-      <b-row>
-        <b-col cols="4" class="text-center ">
-        
-            <a class="nav-link btn btn-secondary" href="#">Restaurant</a>
-
         </b-col>
-          
-        <b-col cols="4" class="text-center ">
-        
-            <a class="nav-link btn btn-secondary" href="#">Arts &amp; Entertainment</a>
-
+      </b-row>
+      <b-form-row class="my-5 py-5">
+        <b-col cols="6">
+          <b-form-input
+            v-model="filter.search"
+            placeholder="Search..."
+          ></b-form-input>
         </b-col>
-          
-        <b-col cols="4" class="text-center ">
-        
-            <a class="nav-link btn btn-secondary" href="#">Retail</a>
-
-        </b-col>          
-    </b-row>      
-    <b-row>
-        <b-col cols="3"></b-col>
-        <b-col cols="3" class="text-center ">
-        
-            <a class="nav-link btn btn-secondary" href="#">Gift Cards</a>
-
+        <b-col cols="3">
+          <div class="border rounded p-3">
+            <b-form-group class="mb-0" label="Filter types">
+              <b-form-checkbox-group
+                v-model="filter.types"
+                :options="types"
+                name="type"
+                stacked
+              ></b-form-checkbox-group>
+            </b-form-group>
+          </div>
         </b-col>
-          
-        <b-col cols="3" class="text-center">
-        
-            <a class="nav-link btn btn-secondary" href="#">Take Out</a>
-
-        </b-col>   
-        
-    </b-row> 
+        <b-col cols="3">
+          <div class="border rounded p-3">
+            <b-form-group class="mb-0" label="Filter types">
+              <b-form-checkbox-group
+                v-model="filter.actions"
+                :options="actions"
+                name="type"
+                stacked
+              ></b-form-checkbox-group>
+            </b-form-group>
+          </div>
+        </b-col>
+      </b-form-row>
     </b-container>
-    
-    <b-container class="padding-50">    
-    <b-row>
-        <b-col cols="4" v-for="(business, idx) in businesses" :key="idx">
+
+    <b-container class="my-5">
+      <b-row>
+        <b-col
+          cols="4"
+          v-for="(business, idx) in filteredBusinessList"
+          :key="idx"
+        >
           <b-card no-body class="mb-2">
             <b-card-header
               :style="{
-                backgroundColor: business.color ? business.color : '#069A78'
+                backgroundColor: '#069A78'
               }"
               header-tag="header"
             >
@@ -75,13 +71,27 @@
               <b-card-text>
                 {{ business.description }}
               </b-card-text>
-                <b-button block :href="business.pageUrl" variant="primary">Gift Cards</b-button>
-                <b-button block :href="business.pageUrl" variant="primary">Takeout / Delivery</b-button>
+              <b-button
+                v-if="business.giftUrl"
+                block
+                :href="business.giftUrl"
+                variant="primary"
+                >Gift Cards</b-button
+              >
+              <b-button
+                v-if="business.takeoutUrl"
+                block
+                :href="business.takeoutUrl"
+                variant="primary"
+                >Takeout / Delivery</b-button
+              >
             </b-card-body>
           </b-card>
         </b-col>
       </b-row>
-            <a class="text-center" href="https://www.helmux.com/work" target="_blank">Built with &#x2665; by Helm Experience &amp; Design</a>
+      <a class="text-center" href="https://www.helmux.com/work" target="_blank"
+        >Built with &#x2665; by Helm Experience &amp; Design</a
+      >
     </b-container>
   </Layout>
 </template>
@@ -110,24 +120,80 @@ export default {
     title: "Home",
     titleTemplate: "Buffalo Gift Card Database"
   },
-  computed: {
-    businesses() {
-      return this.$page.businesses.edges.map(business => {
+  data() {
+    return {
+      filter: {
+        types: [],
+        actions: [],
+        search: ""
+      },
+      types: [],
+      actions: [
+        { text: "Gift Cards", value: "giftUrl" },
+        { text: "Take Out", value: "takeoutUrl" }
+      ],
+      businessList: []
+    };
+  },
+  methods: {
+    initBusinessList() {
+      this.businessList = this.$page.businesses.edges.map(business => {
         var node = business.node;
         return {
           name: node.name,
           description: node.description,
           address: node.address,
-          amounts: node.amounts ? JSON.parse(node.amounts) : null,
           type: node.type,
-          gift_url: new RegExp("^https?://").test(node.gift_url)
+          giftUrl: new RegExp("^https?://").test(node.gift_url)
             ? node.gift_url
             : "http://" + node.gift_url,
-          takeout_url: new RegExp("^https?://").test(node.takeout_url)
+          takeoutUrl: new RegExp("^https?://").test(node.takeout_url)
             ? node.takeout_url
-            : "http://" + node.takeout_url,
+            : "http://" + node.takeout_url
         };
       });
+    },
+    initFilters() {
+      this.businessList.forEach(business => {
+        if (!this.filter.types.includes(business.type) && business.type)
+          this.filter.types.push(business.type);
+      });
+      // this.filter.types = this.types.map(type => {
+      //   return type;
+      // });
+      this.filter.actions = this.actions.map(action => {
+        return action.value;
+      });
+    }
+  },
+  mounted() {
+    this.initBusinessList();
+    this.initFilters();
+  },
+  computed: {
+    filteredBusinessList() {
+      let arr = [];
+      let self = this;
+      arr = this.businessList.filter(business => {
+        var search = self.filter.search.toLowerCase();
+        var name = business.name.toLowerCase();
+        return name.includes(search);
+      });
+
+      if (self.filter.types.length) {
+        arr = arr.filter(business => {
+          return self.filter.types.includes(business.type);
+        });
+      }
+
+      if (self.filter.actions.length) {
+        arr = arr.filter(business => {
+          return self.filter.actions.every(action => {
+            return business[action];
+          });
+        });
+      }
+      return arr;
     }
   }
 };
